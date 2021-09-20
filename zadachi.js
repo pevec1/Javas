023f0ee28b60ea1de83a81eeb8953611a7dba479
pevec1 <pevec1@yandex.ru>
2021-09-20 21:17:01 +0300

    с 4 по 8 задачи

--- zadachi.js
+++ zadachi.js
@@ -1,4 +1,4 @@
-// z4 
+/* z4 */
 let num = +prompt("Введите число: ");
 if (num == 10) alert("Верно");
 else alert("Неверно");
@@ -9,3 +9,27 @@ else alert("Неверно");
 for (let i=1;i<=50;i++){
     console.log(i);
 }
+
+
+/* z6*/
+let a=10;
+let b=3;
+let r = a % b;
+console.log(r);
+
+
+/* z7*/
+let str=String();
+for(i=1;i<=9;i++){
+    str=str+String(i);
+}
+console.log(str);
+
+
+/*z8*/
+let str2=String();
+str2='abcde';
+console.log(str2[0]+" "+str2[1]+" "+str2[4]);
+
+
+
